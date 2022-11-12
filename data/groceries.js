e7168d0b0547ac482fb4605c62ab9cfb84a28857
juli <julitati25@gmail.com>
2022-11-12 11:09:31 +0400

    add grocery list

---
+++ data/groceries.js
@@ -0,0 +1,141 @@
+const groceries = {
+  protein: [
+    {
+      id: 1111,
+      name: "Brazilan meat",
+      producer: "Brazil",
+      price: "$10.99",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 1112,
+      name: "Tuna (200gr)",
+      price: "$6.99",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 1113,
+      name: "Salmon (300gr)",
+      price: "$13.99",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 1114,
+      name: "Eggs 12 units",
+      producer: "USA",
+      price: "$2.5",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    }
+  ],
+  dairy: [
+    {
+      id: 2111,
+      name: "Coconut Milk",
+      producer: "UK",
+      price: "$5.99",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 2112,
+      name: "Cow Milk",
+      producer: "USA",
+      price: "$2.99",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 2113,
+      name: "Cheddar cheese (250gr)",
+      producer: "USA",
+      price: "$2.99",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    }
+  ],
+  fruits: [
+    {
+      id: 3111,
+      name: "Bananas (5 aprox)",
+      producer: "USA",
+      price: "$3.99",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 3112,
+      name: "Apple",
+      price: "$0.99",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 3113,
+      name: "Orange",
+      price: "$0.59",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    }
+  ],
+  beverages: [
+    {
+      id: 4111,
+      name: "Coca cola (500ml)",
+      price: "$0.99",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 4112,
+      name: "Sprite (500ml)",
+      price: "$0.99",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 4113,
+      name: "Chocolate milk (500ml)",
+      price: "$0.59",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    }
+  ],
+  snacks: [
+    {
+      id: 5111,
+      name: "Doritos (500gr)",
+      price: "$2.99",
+      producer: "Canada",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 5112,
+      name: "Ruffles (500gr)",
+      price: "$5.99",
+      producer: "UK",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 5113,
+      name: "Lays (200gr)",
+      price: "$3.99",
+      producer: "UK",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 5114,
+      name: "Pringles (200gr)",
+      price: "$3.99",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    },
+    {
+      id: 5115,
+      name: "Cheetos (500gr)",
+      price: "$2.99",
+      producer: "USA",
+      img: "https://media.wired.com/photos/5b493b6b0ea5ef37fa24f6f6/125:94/w_2393,h_1800,c_limit/meat-80049790.jpg"
+    }
+
+  ]
+};
+
+export default groceries;
