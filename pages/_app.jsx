8ac022489e83c242593a88b32afbc0fca219ee87
juli <julitati25@gmail.com>
2022-12-08 00:59:39 +0300

    add store provider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,14 @@
 import { instanceOf } from "prop-types";
 import React from "react";
+import { Provider } from "react-redux";
+import store from "../store";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => (
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+);
 
 App.propTypes = {
   Component: instanceOf(Object).isRequired,
